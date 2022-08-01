a60c75417c9f76891ae8f3d258b5f9ea51939a32
Tristan Dickson <tristan.dickson@efficioconsulting.com>
2022-08-01 13:11:06 +0100

    added summary download functionality

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
-import puppeteer from "puppeteer";
-import fs from 'fs';
 import Axios from 'axios';
+import fs from 'fs';
+import puppeteer, { Page } from "puppeteer";
+import "reflect-metadata";
 import Tesseract from 'tesseract.js';
 
 const url =
-  "https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=REGION%5E603&minBedrooms=3&maxPrice=375000&radius=20.0&propertyTypes=detached%2Csemi-detached%2Cterraced&primaryDisplayPropertyType=houses&includeSSTC=false&mustHave=&dontShow=retirement%2CsharedOwnership&furnishTypes=&keywords=";
+  "https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=REGION%5E603&minBedrooms=3&maxPrice=375000&radius=20.0&sortType=1&propertyTypes=detached%2Csemi-detached%2Cterraced&primaryDisplayPropertyType=houses&includeSSTC=false&mustHave=&dontShow=retirement%2CsharedOwnership&furnishTypes=&keywords=";
 
 async function downloadImage(url: string, filepath: string) {
   const response = await Axios({
@@ -19,39 +20,90 @@ async function downloadImage(url: string, filepath: string) {
   });
 }
 
-(async () => {
+async function getSummary(page: Page, url: string) {
+  await page.goto(url);
+  // Key Info
+  const address = await page.$eval('h1[itemprop="streetAddress"]', el => el.textContent)
+  console.log(`Address: ${address}`)
+  const price = await page.$eval('div[data-skip-to-content] article div[style] span', el => el.textContent)
+  console.log(`Price: ${price}`)
+  // Summary Info
+  const infoReel = await page.$$('div[data-test="infoReel"] p')
+  const infoReelKeys = ['Property Type', 'Bedrooms', 'Bathrooms', 'Tenure']
+  for (let i = 0; i < infoReel.length; i++) {
+    let value = await infoReel[i].evaluate(el => el.innerHTML)
+    console.log(`${infoReelKeys[i]}: ${value}`)
+  }
+  // Station Info
+  const stationInfo = await page.$$('#Stations-panel li span')
+  const stationInfoKeys = ['Nearest Station Name', 'Nearest Station Distance']
+  for (let i = 0; i < stationInfoKeys.length; i++) {
+    let value = await stationInfo[i].evaluate(el => el.innerHTML)
+    console.log(`${stationInfoKeys[i]}: ${value}`)
+  }
+}
+
+async function getFloorplan(page: Page, url: string, filename: string) {
+  await page.goto(url);
+  const image = await page.$('[alt^="Floorplan"]');
+  if (image) {
+    const imageURL = await page.evaluate(el => el.getAttribute("src"), image);
+    console.log(`Image url: ${imageURL}`);
+    if (imageURL) {
+      await downloadImage(imageURL, filename)
+
+      let result = await Tesseract.recognize(
+        filename,
+        'eng'
+      )
+      let text = result.data.text;
+
+      // console.log(text)
+      const match = text.match(/((([5-9]\d)|(\d{3}))\.\d{1,2}) (sq|SQ)[\s\.]*(m|M|metres|METRES)/)
+      console.log(match ? match[1] : "No match");
+    }
+  }
+}
+
+async function main() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
-  const cards = await page.$$(".propertyCard-link");
-  const path = await page.evaluate(el => el.getAttribute('href'), cards[6])
-  if (path) {
-    const split = path?.split("/")
-    const filename = `floorplan-${split[2]}.jpeg`
-    const floorplanURL = `https://rightmove.co.uk/${split[1]}/${split[2]}/floorplan?activePlan=1&channel=RES_BUY`
-    console.log(floorplanURL)
-    await page.goto(floorplanURL);
-    await page.screenshot({ path: 'example.png' });
-    const image = await page.$('[alt^="Floorplan"]');
-    if (image) {
-      const imageLink = await page.evaluate(el => el.getAttribute("src"), image)
-      console.log(imageLink)
-      if (imageLink) {
-        await downloadImage(imageLink, filename)
-
-        let result = await Tesseract.recognize(
-          filename,
-          'eng'
-        )
-        let text = result.data.text
-
-        // console.log(text)
-        const match = text.match(/((([5-9]\d)|(\d{3}))\.\d{1,2}) (sq|SQ)[\s\.]*(m|M|metres|METRES)/)
-        console.log(match ? match[1] : "No match");
+  // await page.screenshot({ path: 'example.png' });
+  const resultCount = await page.$eval('span[data-bind="counter: resultCount, formatter: numberFormatter"]', el => el.textContent)
+  console.log(resultCount)
+  const pagesText = await page.$eval('span[data-bind="text: total"]', el => el.textContent)
+  const pages = pagesText ? parseInt(pagesText) : null
+  if (pages) {
+    console.log(`Total Pages: ${pages}`)
+    for (let p = 0; p < pages; p++) {
+      await page.goto(`${url}&index=${p * 24}`);
+      const paths = await page.$$eval(
+        '.propertyCard:not(.propertyCard--featured) .propertyCard-priceLink',
+        els => els.map(el => el.getAttribute('href'))
+      );
+      for (let i = 0; i < paths.length; i++) {
+        const relativePath = paths[i];
+        if (relativePath) {
+          const propertyURL = `https://rightmove.co.uk${relativePath}`;
+          console.log(`\nParsing property number ${p * 24 + i + 1} of ${resultCount}`)
+          console.log(`\nProperty url: ${propertyURL}`);
+          const propertyID = relativePath?.split("/")[2].slice(0, -1);
+          await getSummary(page, propertyURL);
+          console.log(`Property ID: ${propertyID}`)
+          const floorplanURL = `https://rightmove.co.uk/properties/${propertyID}#/floorplan?activePlan=1&channel=RES_BUY`;
+          const filename = `floorplan-${propertyID}.jpeg`;
+          console.log(`Floorplan url: ${floorplanURL}`);
+          await getFloorplan(page, floorplanURL, filename)
+        }
       }
     }
   }
 
   await browser.close();
+}
+
+(async () => {
+  await main();
 })();
 
